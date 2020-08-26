91ff9c552392090976a62effd18eb17aa0df631a
Olalekan ISMAIL <irolalekan@gmail.com>
2020-08-26 07:44:31 +0100

    render book details page

--- src/layouts/BookDetail.js
+++ src/layouts/BookDetail.js
@@ -7,7 +7,7 @@ import Services from "../network/Services";
 
 
 const BookDetail = () => {
-    const [book, setBook] = useState([]);
+    const [book, setBook] = useState();
     const {id} = useParams();
 
     useEffect(() => {
@@ -20,43 +20,42 @@ const BookDetail = () => {
             }).catch(error => {
             console.log(error)
         })
-    }, [])
-
+    }, []);
+    let characters = '';
+    let details = '';
+    if (book) {
+        characters = book.characters.map(character => <Character character={character} key={character.id}/>)
+        details = (
+            <div className="wrapper">
+                <div className="header">{book.name}</div>
+                <div className="banner-img">
+                    <img src="https://picsum.photos/640/360/?random" alt="Image 1"/>
+                </div>
+                <div className="dates">
+                    <div className="start">
+                        <strong>Released</strong> {book.released}
+                        <span/>
+                    </div>
+                    <div className="ends">
+                        <strong>Publisher</strong> {book.publisher}
+                    </div>
+                </div>
+                <h5 className="section-title h1">Characters</h5>
+                <div className="row">
+                    {characters}
+                </div>
+                <Comments comments={book.comments}/>
+                <div className="footer">
+                    <p>Developed by: Ismummy</p>
+                </div>
+            </div>);
+    }
     return (
         <div className="container-fluid">
             <div className="row">
                 <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <div className="tile">
-                        <div className="wrapper">
-                            <div className="header">Event Title</div>
-
-                            <div className="banner-img">
-                                <img src="https://picsum.photos/640/360/?random" alt="Image 1"/>
-                            </div>
-
-                            <div className="dates">
-                                <div className="start">
-                                    <strong>STARTS</strong> 12:30 JAN 2015
-                                    <span/>
-                                </div>
-                                <div className="ends">
-                                    <strong>ENDS</strong> 14:30 JAN 2015
-                                </div>
-                            </div>
-
-                            <h5 className="section-title h1">Characters</h5>
-                            <div className="row">
-                                <Character character={'jj'}/>
-                                <Character character={'jj'}/>
-                                <Character character={'jj'}/>
-                                <Character character={'jj'}/>
-                                <Character character={'jj'}/>
-                            </div>
-                            <Comments comments={"klk"}/>
-                            <div className="footer">
-                                <p>Ismummy</p>
-                            </div>
-                        </div>
+                        {details}
                     </div>
                 </div>
             </div>
