875524c7389b6729d5e397b89d752bb12fd79899
Olalekan ISMAIL <irolalekan@gmail.com>
2020-08-26 07:30:30 +0100

    render book

--- src/components/books/Book.js
+++ src/components/books/Book.js
@@ -2,25 +2,29 @@ import React from "react";
 import "./Book.css";
 import {Link} from "react-router-dom";
 
-const Book = ({book}) => (
-    <Link to={"/books/" + book.id} className="col-sm-6 col-md-4 col-lg-3 mt-4">\
-        <div className="card">
-            <img className="card-img-top" src="https://picsum.photos/200/150/?random"/>
-            <div className="card-block">
-                <h4 className="card-title">Tawshif Ahsan Khan</h4>
-                <div className="meta">
-                    <a href="#">Friends</a>
+const Book = ({book}) => {
+const authors = book.authors.map(author => <p> {author.name} |</p>)
+    return (
+        <Link to={"/books/" + book.id} className="col-sm-6 col-md-4 col-lg-3 mt-4">\
+            <div className="card">
+                <img className="card-img-top" src="https://picsum.photos/200/150/?random"/>
+                <div className="card-block">
+                    <h4 className="card-title">{book.name}</h4>
+                    <div className="meta">
+                        {authors}
+                    </div>
+                    <div className="card-text">
+                        Published by {book.publisher} at {book.country} with
+                        isbn {book.isbn} in {book.mediaType} of {book.numberOfPages} pages.
+                    </div>
                 </div>
-                <div className="card-text">
-                    Tawshif is a web designer living in Bangladesh.
+                <div className="card-footer">
+                    <span className="float-right">Released: {book.released}</span>
+                    <span><i className=""/>{book.comments_count} Commennts</span>
                 </div>
             </div>
-            <div className="card-footer">
-                <span className="float-right">Joined in 2013</span>
-                <span><i className=""/>75 Friends</span>
-            </div>
-        </div>
-    </Link>
-);
+        </Link>
+    )
+};
 
 export default Book
